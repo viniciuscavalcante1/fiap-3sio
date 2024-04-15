6a11d0852c23189e0e65bfa090a0e1e8628c9bed
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-04-14 22:11:00 -0300

    feat: comments!

---
+++ Fase 3 - Mobile World/cap3TryKotlin/src/Comments.kt
@@ -0,0 +1,27 @@
+// one line comments
+
+fun main() {
+    // number of students in class
+
+    // var to define variables
+    var class1 = 20
+    var class2 = 30
+    var class3 = 10
+
+    var total = class1 + class2 + class3
+
+    // declarations must be inside functions in order to execute
+    // $total is like f-strings {} in python
+    println("total: $total")
+
+    /* block comments
+    yay!
+     */
+
+    class1 = 25
+    class2 = 35
+    class3 = 15
+
+    total = class1 + class2 + class3
+    println("total: $total")
+}
