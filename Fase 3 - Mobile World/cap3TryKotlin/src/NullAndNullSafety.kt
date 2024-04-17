efb422b6800335ecfa201a7c44e2fb60206b6dfd
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-04-16 21:40:09 -0300

    feat: add null safety!

---
+++ Fase 3 - Mobile World/cap3TryKotlin/src/NullAndNullSafety.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    // in order to create nullable variables, we have to insert '?' after the variable type,
+    // and we must specify the variable type when creating null variables, because if we don't,
+    // the inferred type will be "Nothing?".
+    // this is called null safety!
+
+    val test: Nothing? = null
+    var age: Int? = null
+
+}
