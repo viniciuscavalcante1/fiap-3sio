e525cf79b3ade3eb90103242127e8e3d3e35a57f
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-04 20:20:13 -0300

    feat: add ifs

---
+++ Fase 3 - Mobile World/cap3TryKotlin/src/decisionStructure/Ifs.kt
@@ -0,0 +1,20 @@
+package decisionStructure
+
+fun main() {
+    var age = 10
+
+    if (age >= 18) {
+        println("You are an adult")
+    } else {
+        println("You are a minor")
+    }
+
+    var color = "red"
+    if (color == "blue") {
+        println("The color is blue")
+    } else if (color == "green") {
+        println("The color is green")
+    } else {
+        println("The color is red")
+    }
+}
