f3169bb25d1c64c94f47ea5b72d1c898ccf537fe
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-04 20:26:41 -0300

    feat: add when

---
+++ Fase 3 - Mobile World/cap3TryKotlin/src/decisionStructure/when.kt
@@ -0,0 +1,23 @@
+package decisionStructure
+
+fun main() {
+    val grade = 7.5
+    val result = when {
+        grade >= 7.0 -> "Approved"
+        grade >= 4.0 -> "Recovery"
+        else -> "Disapproved"
+    }
+    println(result)
+
+    val number = 2
+    when (number % 2) {
+        0 -> println("Even")
+        else -> println("Odd")
+    }
+
+    val letter = "a"
+    when (letter) {
+        "a", "e", "i", "o", "u" -> println("Vowel")
+        else -> println("Consonant")
+    }
+}
