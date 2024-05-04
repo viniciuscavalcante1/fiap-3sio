3c0f336e94db5e9d92e7376c144c7e9e946e58be
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-04 11:20:42 -0300

    feat: add set

---
+++ Fase 3 - Mobile World/cap3TryKotlin/src/collections/hashSet.kt
@@ -0,0 +1,20 @@
+package collections
+
+fun main() {
+    // hashSet is a collection that contains unique elements
+    // hashSet is also ordered by default, but we can't guarantee the order and it can change
+
+    val movies = HashSet<String>()
+    println("Movies HashSet size: ${movies.size}")
+
+    movies.add("The Godfather")
+    movies.add("Harry Potter")
+
+    println("Contains Harry Potter: ${movies.contains("Harry Potter")}")
+
+    movies.remove("Harry Potter")
+    println("Contains Harry Potter: ${movies.contains("Harry Potter")}")
+    println("Movies HashSet: $movies")
+
+    val cars = setOf("Ferrari", "Lamborghini", "Porsche")
+}
