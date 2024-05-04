72c67684235d906491248ce0477a764bb1e84efe
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-04 18:34:15 -0300

    feat: add new examples

--- Fase 3 - Mobile World/cap3TryKotlin/src/collections/map.kt
+++ Fase 3 - Mobile World/cap3TryKotlin/src/collections/map.kt
@@ -21,4 +21,33 @@ fun main() {
 
     // Preferred way
     println("Table price: ${products["Table"]}")
+
+    // With pair
+    val movies = HashMap<String, Double>()
+    val movieCatalog = mapOf(
+        "The Godfather" to 9.2,
+        "Harry Potter" to 8.0,
+        "The Lord of the Rings" to 9.0
+    )
+    movies.putAll(movieCatalog)
+
+    val books = HashMap<String, Double>()
+    val bookCatalog = mapOf(
+        Pair("The Catcher in the Rye", 9.0),
+        Pair("1984", 8.5),
+        Pair("The Great Gatsby", 8.7)
+    )
+
+    // IF contains key
+    if (books.containsKey("1984")) {
+        println("1984 is in the books HashMap")
+    } else {
+        println("1984 is not in the books HashMap")
+    }
+
+    // Key and value accessing
+    for (book in books) {
+        println(book.key)
+        println(book.value)
+    }
 }
