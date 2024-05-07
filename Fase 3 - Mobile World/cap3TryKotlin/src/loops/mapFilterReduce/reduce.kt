708841e9a3958ae18c9a0601ec4909417e6fb65a
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-07 08:53:45 -0300

    add reduce

--- Fase 3 - Mobile World/cap3TryKotlin/src/loops/mapFilterReduce/reduce.kt
+++ Fase 3 - Mobile World/cap3TryKotlin/src/loops/mapFilterReduce/reduce.kt
@@ -10,6 +10,8 @@ fun main() {
     // the initial value is the first value of the accumulator
     // reduce uses { } to define the lambda function
 
+    // closures are used to define the lambda function
+
     var numbers = listOf(1, 2, 3, 4, 5)
     var sum = numbers.reduce { acc, i -> acc + i }
     println(sum)
