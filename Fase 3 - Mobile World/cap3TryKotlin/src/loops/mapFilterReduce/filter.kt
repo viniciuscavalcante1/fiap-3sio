070b82ae1879ffcb9ea1c3876ad57fdb93729734
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-07 08:31:15 -0300

    filter

--- Fase 3 - Mobile World/cap3TryKotlin/src/loops/mapFilterReduce/filter.kt
+++ Fase 3 - Mobile World/cap3TryKotlin/src/loops/mapFilterReduce/filter.kt
@@ -1,17 +1,17 @@
 package loops.mapFilterReduce
 
 fun main() {
-    var numbers = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+    val numbers = listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     // filter is a lambda function that returns a new list with the elements that satisfy the condition
     // it uses { } to define the lambda function
-    var even = numbers.filter {
+    val even = numbers.filter {
         it % 2 == 0
     }
 
     println(even)
 
-    var fruits = listOf("Apple", "Banana", "Orange", "Mango", "Melon")
-    var fruitsM = fruits.filter {
+    val fruits = listOf("Apple", "Banana", "Orange", "Mango", "Melon")
+    val fruitsM = fruits.filter {
         it.startsWith("M")
     }
     println(fruitsM)
