f3a09ad9500e8c214e9db321de5fde389aaac02d
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-11 18:00:06 -0300

    Add onClickListener in Kotlin

--- fase3/cap5/views/app/src/main/java/br/com/fiap/views/MainActivity.kt
+++ fase3/cap5/views/app/src/main/java/br/com/fiap/views/MainActivity.kt
@@ -2,6 +2,9 @@ package br.com.fiap.views
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val btnClickAqui = findViewById<Button>(R.id.btn_nome)
+        val txvNome = findViewById<TextView>(R.id.txv_nome)
+        val edtNome = findViewById<EditText>(R.id.edt_nome)
+
+        btnClickAqui.setOnClickListener {
+            val nome: String = edtNome.editableText.toString()
+            txvNome.text = nome
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
