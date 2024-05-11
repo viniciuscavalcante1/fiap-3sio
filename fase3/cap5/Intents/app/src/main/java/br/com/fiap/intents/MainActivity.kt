6e0ce100cddb6825c547840e2e4d8baa53676f7d
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-11 19:04:42 -0300

    Add implicit intent

---
+++ fase3/cap5/Intents/app/src/main/java/br/com/fiap/intents/MainActivity.kt
@@ -0,0 +1,31 @@
+package br.com.fiap.intents
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val btnGoogle = findViewById<Button>(R.id.btn_google)
+        val btnNextActivity = findViewById<Button>(R.id.btn_next_activity)
+
+        btnGoogle.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.br"))
+            startActivity(i)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
