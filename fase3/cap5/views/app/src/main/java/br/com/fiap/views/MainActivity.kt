bcf6c3c8b1127cc189e5bbb165c9ef5817fa1068
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-11 17:43:05 -0300

    Add Toast, ids and button function

--- fase3/cap5/views/app/src/main/java/br/com/fiap/views/MainActivity.kt
+++ fase3/cap5/views/app/src/main/java/br/com/fiap/views/MainActivity.kt
@@ -1,6 +1,8 @@
 package br.com.fiap.views
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun onClickAqui(view: View) {
+        Toast.makeText(this, "Foi clicado!", Toast.LENGTH_SHORT).show()
     }
 }
