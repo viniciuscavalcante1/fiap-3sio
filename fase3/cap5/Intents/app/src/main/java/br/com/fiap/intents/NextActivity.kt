d91783a5244a1be76c52af555054915bd27f2aef
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2024-05-11 23:25:25 -0300

    Add explicit intent

--- fase3/cap5/Intents/app/src/main/java/br/com/fiap/intents/NextActivity.kt
+++ fase3/cap5/Intents/app/src/main/java/br/com/fiap/intents/NextActivity.kt
@@ -1,32 +1,16 @@
 package br.com.fiap.intents
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class NextActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-        val btnGoogle = findViewById<Button>(R.id.btn_google)
-        val btnNextActivity = findViewById<Button>(R.id.btn_next_activity)
-
-        btnGoogle.setOnClickListener {
-            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.br"))
-            startActivity(i)
-        }
-
-        btnNextActivity.setOnClickListener {
-            val i = Intent(this, NextActivity::class.java)
-            startActivity(i)
-        }
+        setContentView(R.layout.activity_next)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
